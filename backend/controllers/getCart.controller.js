3b8a836dd15522be603772478a6551e4712900c6
f4iyaaz <faiyaazismail@gmail.com>
2025-10-21 20:21:26 +0600

    getCart

--- backend/controllers/getCart.controller.js
+++ backend/controllers/getCart.controller.js
@@ -1,10 +1,17 @@
 const { Cart } = require("../models/Cart");
+const { Book } = require("../models/Book");
 
 const getCart = async (req, res) => {
   try {
     const { _id, fullName } = req.user;
     const cart = await Cart.findOne({ user: _id });
-    return res.status(200).send({ cart });
+    const cartItems = cart["items"];
+    let books = [];
+    for (const item of cartItems) {
+      const book = await Book.findOne({ _id: item["book"] });
+      books.push(book["title"]);
+    }
+    return res.status(200).send({ user: fullName, books: books });
   } catch (err) {
     return res.status(400).send(err);
   }
